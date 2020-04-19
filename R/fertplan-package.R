1e142b9d30076ee4724611fa9b951bb24d06ba0f
Marco Bascietto <marco.bascietto@gmail.com>
2020-04-19 07:38:54 +0200

    Added documentation

--- R/fertplan-package.R
+++ R/fertplan-package.R
@@ -1,1 +1,19 @@
-#load("R/sysdata.rda")
+#' fertplan
+#'
+#' fertplan is a small R package to be used in aiding the computation of
+#' fertilization plans for erbaceous and perennial crops.
+#' fertplan computes nitrogen, phosphorus (P2O5), and potassium (K2O)
+#' concentrations to be supplied to field crops.
+#'
+#' It strictly follows the regulation for integrated crop production
+#' published by the administrative authority of Lazio region, Italy.
+#' Assessorato Agricoltura, Promozione della Filiera e della Cultura del
+#' Cibo, Ambiente e Risorse Naturali, 2020. Parte agronomica, norme generali,
+#' Disciplinare di Produzione Integrata della Regione Lazio - SQNPI.
+#' Regione Lazio, Roma, Italy.
+#' \url{http://www.regione.lazio.it/rl_agricoltura/?vw=documentazioneDettaglio&id=52065}
+#'
+#' @docType package
+#' @name fertplan
+#' @author Marco Bascietto <marco.bascietto@@crea.gov.it>
+NULL

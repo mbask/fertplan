9ad3f3275b2d29e34bf3a087dc9f7b93c4dc796d
Marco Bascietto <marco.bascietto@gmail.com>
2020-04-19 08:00:37 +0200

    Fixed typo

--- R/fertplan-package.R
+++ R/fertplan-package.R
@@ -11,7 +11,6 @@
 #' Cibo, Ambiente e Risorse Naturali, 2020. Parte agronomica, norme generali,
 #' Disciplinare di Produzione Integrata della Regione Lazio - SQNPI.
 #' Regione Lazio, Roma, Italy.
-<<<<<<<<< Temporary merge branch 1
 #' \url{http://www.regione.lazio.it/rl_agricoltura/?vw=documentazioneDettaglio&id=52065}
 #'
 #' @docType package

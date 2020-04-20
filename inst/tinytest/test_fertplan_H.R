9d31def34ed7d45683c88958b3b1163773c8e309
Marco Bascietto <marco.bascietto@gmail.com>
2020-04-20 10:41:19 +0200

    Ensured component H

--- inst/tinytest/test_fertplan_H.R
+++ inst/tinytest/test_fertplan_H.R
@@ -2,6 +2,6 @@
 
 expect_equal(H_K_leaching(100), 10)
 expect_equal(H_K_leaching(c(0, 4, 5, 10, 26)), c(60, 60, 60, 30, 10))
-expect_error(H_K_leaching(150), "sum\\(clay_pc > 100\\) == 0 is not TRUE")
-expect_error(H_K_leaching(c(0, 4, 5, 190, 26)), "sum\\(clay_pc > 100\\) == 0 is not TRUE")
-expect_error(H_K_leaching(-8), "sum\\(clay_pc < 0\\) == 0 is not TRUE")
+expect_error(H_K_leaching(150), "all percentages in vector should be \\[0,100\\]")
+expect_error(H_K_leaching(c(0, 4, 5, 190, 26)), "all percentages in vector should be \\[0,100\\]")
+expect_error(H_K_leaching(-8), "all percentages in vector should be \\[0,100\\]")
